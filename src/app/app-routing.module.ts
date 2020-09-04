f5c9fd097941d2a189f9024b77a7eca4129b1af5
pedro-guimaraes00 <pedrogmendes00@gmail.com>
2020-09-04 20:16:17 -0300

    cria restaurants component e sua rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component'
+import { RestaurantsComponent } from './restaurants/restaurants.component'
+
 
 
 const routes: Routes = [
 
-    { path: 'about', component: AboutComponent}
+  { path: 'about', component: AboutComponent },
+  { path: 'restaurants', component: RestaurantsComponent }
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AboutComponent
+    AboutComponent,
+    RestaurantsComponent
   ],
   imports: [
     BrowserModule,

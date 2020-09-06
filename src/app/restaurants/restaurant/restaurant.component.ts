85536cddb740e55a9e2b942200d75003a17e0ed3
pedro-guimaraes00 <pedrogmendes00@gmail.com>
2020-09-06 18:36:06 -0300

    instala json-server, cria db.json e model de Restaurant

--- src/app/restaurants/restaurant/restaurant.component.ts
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Restaurant } from './restaurant.model'
 
 @Component({
   selector: 'app-restaurant',
   templateUrl: './restaurant.component.html',
 })
 export class RestaurantComponent implements OnInit {
+
+  restaurant: Restaurant[]
 
   constructor() { }
 

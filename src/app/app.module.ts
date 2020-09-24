1517aa9d71aff41ba607d6e5e6b8a1a2ff209612
pedro-guimaraes00 <pedrogmendes00@gmail.com>
2020-09-24 15:14:06 -0300

    instala e configura módulo de animações do Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,6 +31,7 @@ import { RestaurantsService } from './restaurants/restaurants.service';
   imports: [
     BrowserModule,
     HttpClientModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
   ],
   

822c53acf80b68db1e019565a547d6be4084a92a
pedro-guimaraes00 <pedrogmendes00@gmail.com>
2020-10-01 20:46:40 -0300

    corrige exibição do component shopping-cart

--- src/app/restaurant-info/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-info/shopping-cart/shopping-cart.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ShoppingCartService } from './shopping-cart.service'
-import { trigger, state, style, transition, animate, keyframes } from '@angular/animations'
+import { trigger, state, style, transition, animate } from '@angular/animations'
 
 @Component({
   selector: 'app-shopping-cart',
@@ -9,24 +9,29 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
   animations: [
     trigger('row', [
       state('ready', style({opacity: 1})),
-      transition('void => ready', animate('500ms 0s ease-in', keyframes([
-        style({ opacity: 0, transform: 'translateX(-30px)', offset: 0 }),
-        style({ opacity: 0.8, transform: 'translateX(15px)', offset: 0.8 }),
-        style({ opacity:1, transform: 'translateX(0px)', offset: 1  })
-      ]))),
-      transition('ready => void', animate('500ms 0s ease-out', keyframes([
-        style({ opacity: 1, transform: 'translateX(-30px)', offset: 1 }),
-        style({ opacity: 0.2, transform: 'translateX(15px)', offset: 0.2 }),
-        style({ opacity:0, transform: 'translateX(0px)', offset: 0  })
-      ])))
+      transition('void => ready', [
+        style({opacity: 0, transform: 'translateX(-20px)'}),
+        animate('500ms 0s ease-in')
+      ])
+    ]),
+
+    trigger('showCart', [
+      state('ready', style({opacity: 1})),
+      transition('void => ready', [
+        style({opacity: 0, transform: 'translateY(-30px)'}),
+        animate('500ms 0s ease-in-out')
+      ])
     ])
   ]  
+
 })
   
   
 export class ShoppingCartComponent implements OnInit {
 
   rowState = 'ready'
+
+  cartState = 'ready'
 
   constructor( private shoppingCartService: ShoppingCartService) { }
 

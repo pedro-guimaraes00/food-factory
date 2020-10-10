6e4c875bb9b05b1601fb7ce65ab66729e1bacd9e
pedro-guimaraes00 <pedrogmendes00@gmail.com>
2020-10-10 17:29:41 -0300

    cria section para dados do usuário e tabela com itens do pedido

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from '../restaurant-info/shopping-cart/shopping-cart.service';
+import { ShoppingCartItem } from '../restaurant-info/shopping-cart/shopping-cart.model'
 
 @Component({
   selector: 'app-order',
@@ -6,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private shoppingCart: ShoppingCartService) { }
 
   ngOnInit(): void {
   }

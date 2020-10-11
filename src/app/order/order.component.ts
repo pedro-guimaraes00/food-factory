8727ed9e9ca1ec1974e84e9c7343f0168078c0c8
pedro-guimaraes00 <pedrogmendes00@gmail.com>
2020-10-11 00:48:25 -0300

    implementa reactive form ao component de pedido

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ShoppingCartService } from '../restaurant-info/shopping-cart/shopping-cart.service';
 import { ShoppingCartItem } from '../restaurant-info/shopping-cart/shopping-cart.model'
+import { FormBuilder, FormGroup, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-order',
@@ -8,9 +9,22 @@ import { ShoppingCartItem } from '../restaurant-info/shopping-cart/shopping-cart
 })
 export class OrderComponent implements OnInit {
 
-  constructor(private shoppingCart: ShoppingCartService) { }
+  formOrder: FormGroup
+
+  constructor(private shoppingCart: ShoppingCartService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.formOrder = this.formBuilder.group({
+      endereco: this.formBuilder.control('', [Validators.required]),
+      numero: this.formBuilder.control('', [Validators.required]),
+      complemento: this.formBuilder.control('', [Validators.required]),
+      nome: this.formBuilder.control('', [Validators.required]),
+      email: this.formBuilder.control('', [Validators.required]),
+      celular: this.formBuilder.control('', [Validators.required]),
+
+    })
+    
   }
 
 }

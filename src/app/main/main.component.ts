278f2d77959eab7d1309d287b2ccc3210126985b
pedro-guimaraes00 <pedrogmendes00@gmail.com>
2020-10-20 19:13:52 -0300

    animação de texto no main component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations'
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
+  animations: [
+    trigger('searchToggle', [
+      state('ready', style({opacity: 1})),
+      transition('void => ready', [
+        style({opacity: 0, transform: 'translateY(-30px)'}),
+        animate('500ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
+  
+  
 export class MainComponent implements OnInit {
+
+  searchState = 'ready'
 
   constructor() { }
 
